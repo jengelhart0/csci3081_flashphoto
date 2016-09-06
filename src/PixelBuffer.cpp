09bb24beeca150aca358d2c842f3acaa661b8783
John Harwell <john.r.harwell@gmail.com>
2016-09-06 17:27:46 -0500

    Clean up iteration 1 base code on its way to being compliant with the google C++ style guide.

--- src/PixelBuffer.cpp
+++ src/PixelBuffer.cpp
@@ -1,76 +1,81 @@
 //
 //  PixelBuffer.cpp
-//  Originally created by the CSci-3081W TAs.
+//  Copyright 2016 CSci-3081W TAs.
 //
+#include "PixelBuffer.h"
+#include "ColorData.h"
 
 #include <iostream>
 #include <cstring>
-#include "PixelBuffer.h"
-#include "ColorData.h"
 
 using std::cerr;
 using std::endl;
 using std::fill;
 
-PixelBuffer::PixelBuffer(int w, int h, ColorData backgroundColor) : m_width(w), m_height(h) {
-	m_backgroundColor = new ColorData(backgroundColor);
-	m_pixels = new ColorData[w*h];
-	fillPixelBufferWithColor(backgroundColor);
+PixelBuffer::PixelBuffer(int w,
+                         int h,
+                         ColorData backgroundColor) : m_width(w),
+                                                      m_height(h) {
+    m_backgroundColor = new ColorData(backgroundColor);
+    m_pixels = new ColorData[w*h];
+    fillPixelBufferWithColor(backgroundColor);
 }
 
 PixelBuffer::~PixelBuffer() {
-	delete [] m_pixels;
-	delete m_backgroundColor;
+    delete [] m_pixels;
+    delete m_backgroundColor;
 }
 
 ColorData PixelBuffer::getPixel(int x, int y) const {
-	ColorData pixelData;
+    ColorData pixelData;
 
-	if ((x < 0) || (x >= m_width) || (y < 0) || (y >= m_height)) {
-		cerr << "getPixel: x,y out of range: " << x << " " << y << endl;
-	}
-	else {
-		int index = x + m_width*(y);
-		pixelData = m_pixels[index];
-	}
-	return pixelData;
+    if ((x < 0) || (x >= m_width) || (y < 0) || (y >= m_height)) {
+        cerr << "getPixel: x,y out of range: " << x << " " << y << endl;
+    } else {
+        int index = x + m_width*(y);
+        pixelData = m_pixels[index];
+    }
+    return pixelData;
 }
 
 void PixelBuffer::setPixel(int x, int y, const ColorData& newPixel) {
-	if ((x < 0) || (x >= m_width) || (y < 0) || (y >= m_height)) {
-		cerr << "setPixel: x,y out of range: " << x << " " << y << endl;
-	}
-	else {
-		int index = x + m_width*(y); //x + m_width*(m_height-(y+1));
-		m_pixels[index] = newPixel;
-	}
+    if ((x < 0) || (x >= m_width) || (y < 0) || (y >= m_height)) {
+        cerr << "setPixel: x,y out of range: " << x << " " << y << endl;
+    } else {
+        int index = x + m_width*(y);  // x + m_width*(m_height-(y+1));
+        m_pixels[index] = newPixel;
+    }
 }
 
 ColorData const * PixelBuffer::getData() const {
-	return m_pixels;
+    return m_pixels;
 }
 
 int PixelBuffer::getHeight() const {
-	return m_height;
+    return m_height;
 }
 
 int PixelBuffer::getWidth() const {
-	return m_width;
+    return m_width;
 }
 
 ColorData PixelBuffer::getBackgroundColor() {
-	return *m_backgroundColor;
+    return *m_backgroundColor;
 }
 
 void PixelBuffer::fillPixelBufferWithColor(ColorData color) {
-	fill(m_pixels, m_pixels+m_width*m_height, color);
+    fill(m_pixels, m_pixels+m_width*m_height, color);
 }
 
-void PixelBuffer::copyPixelBuffer(PixelBuffer * sourceBuffer, PixelBuffer * destinationBuffer) {
-	if (destinationBuffer->getWidth() != sourceBuffer->getWidth() || destinationBuffer->getHeight() != sourceBuffer->getHeight()) {
-		cerr << "copyPixelBuffer: " << "dimension mismatch" << endl;
-	}
-	else {
-		memcpy ( (void*)destinationBuffer->m_pixels, (void*) sourceBuffer->m_pixels, sizeof(ColorData)*destinationBuffer->m_height*destinationBuffer->m_width );
-        }
+void PixelBuffer::copyPixelBuffer(
+    PixelBuffer *sourceBuffer,
+    PixelBuffer *destBuffer) {
+    if (destBuffer->getWidth() != sourceBuffer->getWidth() ||
+        destBuffer->getHeight() != sourceBuffer->getHeight()) {
+        cerr << "copyPixelBuffer: " << "dimension mismatch" << endl;
+    } else {
+        memcpy(reinterpret_cast<void*>destBuffer->m_pixels,
+               reinterpret_cast<void*>sourceBuffer->m_pixels,
+               sizeof(ColorData)*destBuffer->m_height*destBuffer->m_width);
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Originally created by the CSci-3081W TAs.
+//  Copyright 2016 CSci-3081W TAs.
 //
 
 
@@ -8,9 +8,10 @@
 #include "ColorData.h"
 
 int main(int argc, char* argv[]) {
-	BrushWorkApp *app = new BrushWorkApp(argc, argv, 800, 800, ColorData(1,1,0.95));
-	// runMainLoop returns when the user closes the graphics window.
-	app->runMainLoop();
-	delete app;
-	exit(0);
+        BrushWorkApp *app = new BrushWorkApp(argc, argv, 800, 800,
+                                             ColorData(1, 1, 0.95));
+        // runMainLoop returns when the user closes the graphics window.
+        app->runMainLoop();
+        delete app;
+        exit(0);
 }
