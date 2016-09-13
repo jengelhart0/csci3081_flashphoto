5e88d1cd059d5caf2f3267f43dcd72390628eb60
Seth Johnson <joh08230@umn.edi>
2016-09-13 12:31:45 -0500

    style(iter1-base) Fixed some camel case variable and param names

--- src/pixel_buffer.cc
+++ src/pixel_buffer.cc
@@ -30,12 +30,12 @@ namespace image_tools {
  ******************************************************************************/
 PixelBuffer::PixelBuffer(int w,
                          int h,
-                         ColorData backgroundColor)
+                         ColorData background_color)
     : width_(w),
       height_(h),
       pixels_(new ColorData[w*h]),
-      background_color_(new ColorData(backgroundColor)) {
-    FillPixelBufferWithColor(backgroundColor);
+      background_color_(new ColorData(background_color)) {
+    FillPixelBufferWithColor(background_color);
 }
 
 PixelBuffer::~PixelBuffer(void) {
@@ -47,23 +47,23 @@ PixelBuffer::~PixelBuffer(void) {
  * Member Functions
  ******************************************************************************/
 ColorData PixelBuffer::get_pixel(int x, int y) const {
-    ColorData pixelData;
+    ColorData pixel_data;
 
     if ((x < 0) || (x >= width_) || (y < 0) || (y >= height_)) {
         cerr << "getPixel: x,y out of range: " << x << " " << y << endl;
     } else {
         int index = x + width_*(y);
-        pixelData = pixels_[index];
+        pixel_data = pixels_[index];
     }
-    return pixelData;
+    return pixel_data;
 }
 
-void PixelBuffer::set_pixel(int x, int y, const ColorData& newPixel) {
+void PixelBuffer::set_pixel(int x, int y, const ColorData& new_pixel) {
     if ((x < 0) || (x >= width_) || (y < 0) || (y >= height_)) {
         cerr << "setPixel: x,y out of range: " << x << " " << y << endl;
     } else {
         int index = x + width_*(y);  // x + width*(height-(y+1));
-        pixels_[index] = newPixel;
+        pixels_[index] = new_pixel;
     }
 }
 
