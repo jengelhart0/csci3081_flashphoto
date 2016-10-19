1919a2eefefa12101e501153ba25b3644fa3b7a4
James Stanley <stan0611@umn.edu>
2016-10-19 15:33:12 -0500

    Adding highlighter implementation. In order to get a more true highlighter look, Highlighter implements its own Draw() function that is identical to Tool::Draw() in every way except that the intensity considers the luminance of the canvas colors.

--- include/highlighter.h
+++ include/highlighter.h
@@ -29,7 +29,7 @@ namespace image_tools {
  * @brief This parent class holds the default implementation for Draw() 
  * as well the pixel mask but nothing else.
  */
-class Highlighter {
+class Highlighter : public Tool {
  public:
     Highlighter(void);
     virtual ~Highlighter(void);
