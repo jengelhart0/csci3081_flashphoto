d703b2f2b2e145b15819193ef48cd2924cfd6f87
James Stanley <stan0611@umn.edu>
2016-10-19 15:40:37 -0500

    Adds our "special" brush - WireBrush. This tool uses pseudo-random generation to decide whether each pixel in the mask is given a value of 0 or 1. This simulates the effect of a wire brush.

---
+++ src/wire_brush.cc
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Name            : wire_brush.cc
+ * Project         : BrushWork
+ * Module          : image_tools
+ * Description     : Implementation of WireBrush class
+ * Copyright       : 2016 CSCI3081W Group C07. All rights reserved.
+ * Creation Date   : 10/18/2016
+ * Original Author : James Stanley
+ *
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <stdlib.h>
+#include "include/wire_brush.h"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+namespace image_tools {
+
+/*******************************************************************************
+ * Constructors/Destructors
+ ******************************************************************************/
+WireBrush::WireBrush(void) : Tool(35, 35) {
+    calculate_mask();
+}
+
+WireBrush::~WireBrush(void) {}
+
+/*******************************************************************************
+ * Member Functions
+ ******************************************************************************/
+void WireBrush::calculate_mask(void) {
+    int height = Tool::height();
+    int length = Tool::length();
+    int size = height * length;
+    float new_mask[35*35];
+    for (int i = 0; i < size; i++) {
+        if ((rand() % 100) <= 10) {
+            new_mask[i] = 1.0;
+        } else {
+            new_mask[i] = 0.0;
+        }
+    }
+    Tool::mask(new_mask);
+}
+}  // namespace image_tools
