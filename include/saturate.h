af5bca636649c4a29a2507d91810091203ed6b45
James Stanley <stan0611@umn.edu>
2016-11-21 16:24:16 -0600

    This adds the saturation filter. This filter makes a gray-scale copy of the canvas, then interpolates between that and the actual canvas using the saturation amount. Values between 0 and 1 will gray-out the image,     while values greater than 1 will saturate the colors by a factor     equal to the saturation value. Values less than 0 will invert the     colors.

---
+++ include/saturate.h
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Name            : saturate.h
+ * Project         : FlashPhoto
+ * Module          : utils
+ * Description     : Header file for Saturate class.
+ * Copyright       : 2016 CSCI3081W - Group C07. All rights reserved.
+ * Creation Date   : 11/07/2016
+ * Original Author : James Stanley
+ *
+ ******************************************************************************/
+
+#ifndef INCLUDE_SATURATE_H_
+#define INCLUDE_SATURATE_H_
+/*******************************************************************************
+ * Includes
+ *******************************************************************************/
+#include "include/pixel_buffer.h"
+#include "include/filter.h"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+namespace image_tools {
+
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+/**
+ * @brief This abstract base class from which all Saturates inherit defines the  
+ *        default implementation for ApplySaturate() and declares the interface for ModifyPixel().
+ */
+
+class Saturate : public Filter {
+ public:
+      explicit Saturate(PixelBuffer *canvas);
+      virtual ~Saturate(void);
+
+      /*
+       * @brief Set saturation value 
+       */
+      void saturation(float saturation) { saturation_ = saturation; }
+
+      /* 
+       * @brief Interpolate between grayscale image and original based on 
+       * saturation value
+       */
+      void ModifyPixel(int x, int y);
+
+ private:
+      float saturation_;
+};
+}  // namespace image_tools
+
+#endif  // INCLUDE_SATURATE_H_
