2b9ea850431ff8d698743ba67981872f9f70f203
James Stanley <stan0611@umn.edu>
2016-11-21 16:10:29 -0600

    This commit adds the quantize filter. This filter works by rounding each pixel's color channel values to it's nearest "bin" value. For example,     if there are 4 bins, there are only 4 possible values a color     channel can have: 0.0, 0.33, 0.66, and 1.0. This behavior is     implemented in Quantize::ModifyPixel().

---
+++ include/quantize.h
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Name            : quantize.h
+ * Project         : FlashPhoto
+ * Module          : utils
+ * Description     : Header file for Quantize class.
+ * Copyright       : 2016 CSCI3081W - Group C07. All rights reserved.
+ * Creation Date   : 11/15/2016
+ * Original Author : James Stanley
+ *
+ ******************************************************************************/
+
+#ifndef INCLUDE_QUANTIZE_H_
+#define INCLUDE_QUANTIZE_H_
+/*******************************************************************************
+ * Includes
+ *******************************************************************************/
+#include "include/pixel_buffer.h"
+#include "include/filter.h"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+namespace image_tools {
+
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+/**
+ * @brief This abstract base class from which all Quantizes inherit defines the  
+ *        default implementation for ApplyQuantize() and declares the interface for ModifyPixel().
+ */
+
+class Quantize : public Filter {
+ public:
+      explicit Quantize(PixelBuffer *canvas);
+      virtual ~Quantize(void);
+
+      /*
+       * @brief Setter method quantize value
+       */
+      void bins(int bins) { bins_ = bins; }
+
+      /* 
+       * @brief Adjust each color channel based on number of bins
+       */
+      void ModifyPixel(int x, int y);
+
+ private:
+      int bins_;
+};
+}  // namespace image_tools
+
+#endif    // INCLUDE_QUANTIZE_H_
